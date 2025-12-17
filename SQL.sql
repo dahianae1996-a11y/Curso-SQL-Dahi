9b5f169e09feaa010a34a9f2db12152cc582d7b1
dahianae1996-a11y <dahianae1996@gmail.com>
2025-12-16 21:48:13 -0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -79,5 +79,157 @@ CREATE TABLE Turno (
     FOREIGN KEY (id_vet) REFERENCES Veterinaria(id_vet),
     FOREIGN KEY (id_servicio) REFERENCES Servicio(id_servicio),
     FOREIGN KEY (id_metodo_pago) REFERENCES Metodo_pago(id_metodo_pago));
+USE Base_Vet;
+##------- Crear Vistas -------
+
+## veterinarias_por_zona
+
+CREATE VIEW veterinarias_por_zona AS
+SELECT 
+    v.id_vet,
+    v.nombre_vet,
+    v.direccion,
+    z.nombre_zona,
+    z.ciudad
+FROM Veterinaria v
+JOIN Zona z ON v.id_zona = z.id_zona;
+
+## socios_y_mascotas
+
+CREATE VIEW socios_y_mascotas AS
+SELECT 
+    s.id_socio,
+    s.nombre,
+    s.apellido,
+    m.id_mascota,
+    m.nombre AS nombre_mascota,
+    m.especie,
+    m.raza
+FROM Socio s
+JOIN Mascota m ON s.id_socio = m.id_socio;
+
+## turnos_detalle
+
+CREATE VIEW turnos_detalle AS
+SELECT 
+    t.id_turno,
+    t.fecha_hora,
+    m.nombre AS mascota,
+    v.nombre_vet,
+    s.nombre_servicio,
+    s.costo,
+    mp.banco
+FROM Turno t
+JOIN Mascota m ON t.id_mascota = m.id_mascota
+JOIN Veterinaria v ON t.id_vet = v.id_vet
+JOIN Servicio s ON t.id_servicio = s.id_servicio
+JOIN Metodo_pago mp ON t.id_metodo_pago = mp.id_metodo_pago;
+
+##------- Funciones -------
+
+## Funcion costo servicio
+
+DELIMITER //
+
+CREATE FUNCTION fn_costo_servicio(idServicio INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    DECLARE costoServicio FLOAT;
+
+    SELECT costo
+    INTO costoServicio
+    FROM Servicio
+    WHERE id_servicio = idServicio;
+
+    RETURN costoServicio;
+END //
+
+DELIMITER ;
+
+## Funcion calcular edad mascota
+
+DELIMITER //
+
+CREATE FUNCTION fn_calcular_edad_mascota(fecha_nac DATE)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    RETURN TIMESTAMPDIFF(YEAR, fecha_nac, CURDATE());
+END //
+
+DELIMITER ;
+
+## -------STORED PROCEDURES-------
+
+## Registrar Turno
+
+DELIMITER //
+
+CREATE PROCEDURE registrar_turno (
+    IN p_fecha_hora DATETIME,
+    IN p_id_mascota INT,
+    IN p_id_vet INT,
+    IN p_id_servicio INT,
+    IN p_id_metodo_pago INT
+)
+BEGIN
+    INSERT INTO Turno (
+        fecha_hora,
+        id_mascota,
+        id_vet,
+        id_servicio,
+        id_metodo_pago
+    )
+    VALUES (
+        p_fecha_hora,
+        p_id_mascota,
+        p_id_vet,
+        p_id_servicio,
+        p_id_metodo_pago
+    );
+END //
+
+DELIMITER ;
+
+## Turno por Veterinaria
+
+DELIMITER //
+
+CREATE PROCEDURE listar_turnos_por_veterinaria (
+    IN p_id_vet INT
+)
+BEGIN
+    SELECT 
+        t.id_turno,
+        t.fecha_hora,
+        m.nombre AS mascota,
+        s.nombre_servicio
+    FROM Turno t
+    JOIN Mascota m ON t.id_mascota = m.id_mascota
+    JOIN Servicio s ON t.id_servicio = s.id_servicio
+    WHERE t.id_vet = p_id_vet;
+END //
+
+DELIMITER ;
+
+## Mascota por Socios
+
+DELIMITER //
+
+CREATE PROCEDURE mascotas_por_socio (
+    IN p_id_socio INT
+)
+BEGIN
+    SELECT 
+        id_mascota,
+        nombre,
+        especie,
+        raza
+    FROM Mascota
+    WHERE id_socio = p_id_socio;
+END //
+
+DELIMITER ;
     
     
